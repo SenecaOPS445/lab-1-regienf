612a5c87ed9d562d970c11e83a004083d8da0c47
Regie Francisco <rfrancisco6@myseneca.ca>
2025-01-10 21:59:32 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This script demonstrates the uses of Python string object.'''
+
+# Define object
+name = 'Isaac'
+
+# Print question using string object
+print('How old are you ' + name + '?')
