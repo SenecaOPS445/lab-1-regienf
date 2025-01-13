e44e898e09bca45102f1e4d0d3df15bff9b8086b
Regie Francisco <rfrancisco6@myseneca.ca>
2025-01-13 16:33:23 -0500

    Fixed the lab1c output error

---
+++ lab1c.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Description: Demonstrates the use of integer objects.'''
+
+# Define two integer objects
+#num1 = 5
+#num2 = 10
+
+# Print the values of the integers
+#print(num1)
+#print(num2)
+
+# Perform addition and store in a new object
+#sum = num1 + num2
+
+# Print the result
+#print(sum)
+
+# Concatenate
+#print('The sum is: ' + str(sum))
+
+# Assign name and age
+name = 'Isaac'
+age = 72
+
+# Print a descriptive message
+print(name + ' is ' + str(age) + ' years old!')
