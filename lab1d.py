52713e338f6d5e1d917a64a6f436e9c67bab6d9e
Regie Francisco <rfrancisco6@myseneca.ca>
2025-01-13 16:53:55 -0500

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: Demonstrates how to perform basic mathematical operations in Python.'''
+
+# Object x,y and z
+x = 10
+y = 2
+z = 5
+
+# Result
+result = x + y * z
+
+# Result
+print(f"{x} + {y} * {z} = {result}")
